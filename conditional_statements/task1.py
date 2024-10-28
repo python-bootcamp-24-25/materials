caf87fe291d91bc2bb9483d29be56cf8cab0c126
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-10-28 21:24:01 +0400

    Add solution of question1

---
+++ conditional_statements/task1.py
@@ -0,0 +1,8 @@
+number = int(input("Enter the number: ")) # tam ədədin (int) istifadəci tərəfindən daxil edilməsi
+
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
