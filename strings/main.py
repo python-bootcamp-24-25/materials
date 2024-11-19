51954ac260b87f27da142f2dddf972ac5f318cb2
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-11-19 18:49:12 +0400

    Add string lecture

---
+++ strings/main.py
@@ -0,0 +1,39 @@
+# name = "Emil"
+
+# name[0] = "A"
+
+#name = "Emil"
+
+#new_name = name.replace("m", "n")
+
+'''print("Hello %s!" % "World") # 1
+
+print("{} {}!".format("Hello", "World")) # 2
+
+name = "Emil" # 3.6
+
+surname = "Emil" 
+
+print(f"Hello {name} {surname}!") # 3'''
+
+print("Hello\nEmil") # \n -> yeni setir cap olunma
+
+print("Hello\tEmil") # \t -> tab
+
+print("Hello \"Emil\"")
+
+print("Hello \\")
+
+'''# \b -> backspace
+
+# \a -> system sound
+
+
+# E m i l -> 4 character
+
+# [1, 2, 3] -> 12 bayt
+
+
+'123' -> '1' '2' '3' --> 3 bayt
+
+[1, 2, 3] -> 12 bayt'''
